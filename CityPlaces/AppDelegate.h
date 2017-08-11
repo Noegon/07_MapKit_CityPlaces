75e1a86302798425643429d2782cdef62146d2ce
Alexey Stafeyev <noegon@yandex.ru>
2017-08-11 20:58:25 +0300

    [INIT]Initial commit

---
+++ CityPlaces/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CityPlaces
+//
+//  Created by Alexey Stafeyev on 11.08.17.
+//  Copyright © 2017 Alexey Stafeyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CityPlaces/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CityPlaces
+//
+//  Created by Alexey Stafeyev on 11.08.17.
+//  Copyright © 2017 Alexey Stafeyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
